d1b84c9226aad655689fd93b2ce3d0274bd918b8
Nadia at AIG <AIGAdmin@USG7L787P9W1.r1-core.r1.aig.net>
2016-08-12 15:31:21 -0400

    added things

---
+++ nancy.js
@@ -0,0 +1,10 @@
+function myFunction() {
+  var image = document.getElementById("gif");
+  if  (image.src== "file:///C:/Users/AIGAdmin/Desktop/nadia.jpg") {
+    image.src = "nadia2.jpg";
+
+}
+  else  {
+  image.src= "nadia.jpg";
+}
+}
